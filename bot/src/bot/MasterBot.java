0bcad7193f11275d01f5aa24e247ac1c124e64d4
Nidhi Narendra <nidhi.narendra1@gmail.com>
2016-10-22 21:20:39 -0700

    Storing the slave data in the hashmap

--- bot/src/bot/MasterBot.java
+++ bot/src/bot/MasterBot.java
@@ -9,16 +9,18 @@ import java.util.*;
 public class MasterBot {
 
 	static class SlaveData{                                                                                               
-        public int port;                                                                                                  
-        public String ip;                                                                                                 
-                                                                                                                          
-        public SlaveData(String ip, int port) {                                                                           
-            this.port = port;                                                                                             
-            this.ip = ip;                                                                                                 
-        }                                                                                                                 
-    }
-	
-		
+		public int port;                                                                                                  
+		public String ip; 
+		public Date registerDate;
+
+		public SlaveData(String ip, int port) {                                                                           
+			this.port = port;                                                                                             
+			this.ip = ip;    
+			this.registerDate = new Date();
+		}                                                                                                                 
+	}
+
+
 	public static void main(String[] args) throws Exception {
 		if (args.length != 2)
 		{
@@ -28,7 +30,7 @@ public class MasterBot {
 
 		Integer masterport;
 		masterport = Integer.parseInt(args[1]);
-		
+
 		try{
 			ServerSocket listener = new ServerSocket(masterport);
 			while (true)
@@ -36,19 +38,15 @@ public class MasterBot {
 				Socket client_socket = listener.accept();
 				PrintWriter out = new PrintWriter(client_socket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-				
+
 				//To get the server's response, EchoClient reads from the BufferedReader object stdIn,
 				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-				
+
 				String dataRecived = in.readLine();
 				System.out.println("Slave says:" + dataRecived);
-		
-			
-				extractSlaveData(dataRecived);
 
-				
-				// extract the slave_ip and slave_port
-				storeSlaveData();
+
+				extractSlaveData(dataRecived);
 			}    
 
 			//DataInputStream from_slave = new6 DataInputStream(serviceSocket.getInoutStream());
@@ -57,39 +55,38 @@ public class MasterBot {
 			System.out.println(e);
 		}
 	}
-	
-	public static void storeSlaveData(){
-		Map<String, String> slaveDataMap = new HashMap <String,String> ();
-		String str;
-		slaveDataMap.put("Nidhi", "Slave1");
-		slaveDataMap.put("Darshan", "Slave2");
-		Set set = slaveDataMap.entrySet();
-	    Iterator iterator = set.iterator();		
 
-	    while(iterator.hasNext()) {
-	         Map.Entry slaveData = (Map.Entry)iterator.next();
-	         System.out.print("key is: "+ slaveData.getKey() + " & Value is: ");
-	         System.out.println(slaveData.getValue());
-	      }
-		
+
+	public static String concatinatedData(SlaveData params){
+		String concatinate = params.ip + params.port;
+		return concatinate;
 	}
-	
+
+
+	public static void storeSlaveData(SlaveData slaveObj){
+		Map<String, SlaveData> slaveDataMap = new HashMap <String,SlaveData> ();
+		String uniqueStr = concatinatedData(slaveObj);
+		slaveDataMap.put(uniqueStr, slaveObj);
+		Set set = slaveDataMap.entrySet();
+		Iterator iterator = set.iterator();		
+
+		while(iterator.hasNext()) {
+			Map.Entry slaveData = (Map.Entry)iterator.next();
+			System.out.print(uniqueStr + "\t" + slaveObj.ip + "\t" + slaveObj.port + "\t" + slaveObj.registerDate);
+			//System.out.println(slaveData.getValue());
+		}
+
+	}
+
 	public static void extractSlaveData(String slaveDataRecieved){
 		String delims = ",";
 		StringTokenizer st = new StringTokenizer(slaveDataRecieved, delims);
-		while (st.hasMoreElements()) {
-			 SlaveData slv1 = new SlaveData((String)st.nextElement(), Integer.parseInt((String) st.nextElement()));
-			System.out.println("StringTokenizer Output: " + st.nextElement());
-		}
+		
+		SlaveData slv1 = new SlaveData((String)st.nextElement(), Integer.parseInt((String) st.nextElement()));
+		storeSlaveData(slv1);
 	}
-	
-	/*
-	 *         SlaveData slv1 = new SlaveData(port_int, ip);
-	        
-	        System.out.println("ip is:" + ip + "port is" + port_int + "slvdata object is" + slv1.toString());
 
-	 */
-	
+
 }
-       
+
 
