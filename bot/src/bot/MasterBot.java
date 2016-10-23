e0473212088663e3e2f9c3f8f03790e7f39880bb
Nidhi Narendra <nidhi.narendra1@gmail.com>
2016-10-22 18:48:56 -0700

    master has a hashmap fucntion

--- bot/src/bot/MasterBot.java
+++ bot/src/bot/MasterBot.java
@@ -1,25 +1,5 @@
 //package bot;
-/*		
-		// create a dictionary with key = SourcePortNumber and value = SlaveHostName, IPAddress, SourcePortNumber, RegistrationDate
-		
-		
-		//list: print all the values in the dictionary
-		
-		
-		/*connect:
-		 * 1. check if the entry exists in the dictionary, if exists error, else continue 
-		 * 2. create a slave process and run it 
-		 * 3. parameters to pass are master PORT and slave id
-		 */
-		 
-		
 
-/*disconnect:
-		 * 1. check if the etry exsits in the disctionary, if not error
-		 * 2. get the slave id and issue a kill command to the slave
-		 * 3. update the dictionary by deleting this entry
-		 */
-		 
 
 import java.io.*;
 import java.net.*;
@@ -45,7 +25,8 @@ public class MasterBot {
 				Socket client_socket = listener.accept();
 				PrintWriter out = new PrintWriter(client_socket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-				System.out.println("Input says:" + in.readLine());
+				System.out.println("Slave says:" + in.readLine());
+				storeSlaveData();
 			}    
 
 			//DataInputStream from_slave = new6 DataInputStream(serviceSocket.getInoutStream());
@@ -54,6 +35,23 @@ public class MasterBot {
 			System.out.println(e);
 		}
 	}
+	
+	public static void storeSlaveData(){
+		Map<String, String> slaveDataMap = new HashMap <String,String> ();
+		String str;
+		slaveDataMap.put("Nidhi", "Slave1");
+		slaveDataMap.put("Darshan", "Slave2");
+		Set set = slaveDataMap.entrySet();
+	    Iterator iterator = set.iterator();		
+
+	    while(iterator.hasNext()) {
+	         Map.Entry slaveData = (Map.Entry)iterator.next();
+	         System.out.print("key is: "+ slaveData.getKey() + " & Value is: ");
+	         System.out.println(slaveData.getValue());
+	      }
+		
+	}
+	
 }
        
 
